97d82d444ac4bbd64968d554e5c79aaaa44b6ccb
Wasee Sun <revengersun@gmail.com>
2025-04-03 17:17:26 +0600

    Added nextjs and django settings fallback

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS
-    ? process.env.ALLOWED_DEV_ORIGINS.split(",")
-    : ["localhost"],
+    ? process.env.ALLOWED_DEV_ORIGINS.split(',')
+    : ['localhost'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: process.env.NEXTJS_IMAGE_HOST,
+        protocol: 'https',
+        hostname: process.env.NEXTJS_IMAGE_HOST || 'localhost',
       },
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "platform-lookaside.fbsbx.com",
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
       },
       {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
     ],
   },
